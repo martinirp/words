90e49597f42f934c7ab849804d6a638542483434
Lucas Rezende <96215982+martinirp@users.noreply.github.com>
2024-01-25 20:31:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 (function(){
     'use strict';
-
+	
+	let $cpf = document.getElementById("content_cpf");
     let $quadro = document.getElementById("content");
     let $clear_button = document.getElementById("clearButton");
     let $uppercaseButton = document.getElementById("uppercaseButton");
     let $lowercaseButton = document.getElementById("lowercaseButton");
+	let $cleanCPFButton = document.getElementById("cpfButton");
 
     $clear_button.addEventListener('click', clean);
     $uppercaseButton.addEventListener('click', upper_case);
     $lowercaseButton.addEventListener('click', lower_case);
+	$cleanCPFButton.addEventListener('click', cpf_clean);
 
     function clean(){
-        $quadro.value = ''
+        $quadro.value = '';
     };
 
     function upper_case(){
-        $quadro.value = $quadro.value.toUpperCase()
+        $quadro.value = $quadro.value.toUpperCase();
     };    
 
     function lower_case(){
-        $quadro.value = $quadro.value.toLowerCase()
+        $quadro.value = $quadro.value.toLowerCase();
     };
+	
+	function cpf_clean(){
+		$cpf.value = $cpf.value.replace(/[^\d]/gi,'');
+	};
 })();
